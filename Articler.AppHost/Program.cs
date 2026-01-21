e9773c2597b02d6a75e62598f563a08f498e2644
TopTuK <sergey.sidorov@pmi.moscow>
2026-01-21 17:02:03 +0300

    add postgres in docker

--- Articler.AppHost/Program.cs
+++ Articler.AppHost/Program.cs
@@ -2,8 +2,9 @@ using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var isDevelopmentMode = builder.Environment.IsDevelopment();
 var startupType = Environment.GetEnvironmentVariable("STARTUP_TYPE")?.ToLower();
-var IsNotDevelopment = !builder.Environment.IsDevelopment();
+var pgMode = Environment.GetEnvironmentVariable("POSTGRES_MODE")?.ToLower() ?? "local";
 
 if (startupType == "docker")
 {
@@ -11,19 +12,53 @@ if (startupType == "docker")
 }
 else
 {
-    var front = builder.AddNpmApp("front", "../webapp", "dev");
+    if (pgMode == "docker")
+    {
+        var pgUser = builder.AddParameter("postgres-user", "articler");
+        var pgPassword = builder.AddParameter("postgres-password", "articler", secret: true);
+        var postgres = builder
+            .AddPostgres("postgres", port: 5432)
+            .WithEnvironment("POSTGRES_USER", "articler") // for scripts
+            .WithEnvironment("POSTGRES_PASSWORD", "articler") // for scripts
+            .WithEnvironment("POSTGRES_DB", "articler") // for scripts
+            .WithUserName(pgUser)
+            .WithPassword(pgPassword)
+            .WithDataVolume("articler_postgres")
+            .WithBindMount("Scripts", "/docker-entrypoint-initdb.d")
+            .AddDatabase("articler");
 
-    var siloHost = builder.AddProject<Projects.Articler_SiloHost>("articler-silohost")
-        .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-        .WithEnvironment("DOTNET_ENVIRONMENT", "Development");
+        var front = builder.AddNpmApp("front", "../webapp", "dev");
 
-    var webApi = builder
-        .AddProject<Projects.Articler_WebApi>("api")
-        .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-        .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
-        .WithReference(siloHost)
-        .WithReference(front)
-        .WaitFor(siloHost);
+        var siloHost = builder.AddProject<Projects.Articler_SiloHost>("articler-silohost")
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+            .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
+            .WithReference(postgres)
+            .WaitFor(postgres);
+
+        var webApi = builder
+            .AddProject<Projects.Articler_WebApi>("api")
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+            .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
+            .WithReference(siloHost)
+            .WithReference(front)
+            .WithHttpsEndpoint(port: 7209)
+            .WaitFor(siloHost);
+    }
+    else
+    {
+        var front = builder.AddNpmApp("front", "../webapp", "dev");
+        var siloHost = builder.AddProject<Projects.Articler_SiloHost>("articler-silohost")
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+            .WithEnvironment("DOTNET_ENVIRONMENT", "Development");
+
+        var webApi = builder
+            .AddProject<Projects.Articler_WebApi>("api")
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+            .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
+            .WithReference(siloHost)
+            .WithReference(front)
+            .WaitFor(siloHost);
+    }
 }
 
 builder
---
+++ Articler.AppHost/Scripts/01-postgresql-main.sql
@@ -0,0 +1,10 @@
+-- This table defines Orleans operational queries. Orleans uses these to manage its operations,
+-- these are the only queries Orleans issues to the database.
+-- These can be redefined (e.g. to provide non-destructive updates) provided the stated interface principles hold.
+CREATE TABLE OrleansQuery
+(
+    QueryKey varchar(64) NOT NULL,
+    QueryText varchar(8000) NOT NULL,
+
+    CONSTRAINT OrleansQuery_Key PRIMARY KEY(QueryKey)
+);
