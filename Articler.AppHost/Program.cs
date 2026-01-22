15deb44842523a14d799ca4244bd2ad8a2a6e9e1
TopTuK <sergey.sidorov@pmi.moscow>
2026-01-22 15:21:34 +0300

    add docker support

--- Articler.AppHost/Program.cs
+++ Articler.AppHost/Program.cs
@@ -8,7 +8,40 @@ var pgMode = Environment.GetEnvironmentVariable("POSTGRES_MODE")?.ToLower() ?? "
 
 if (startupType == "docker")
 {
+    var webAppBuild = builder.AddNpmApp("webapp-build", "../webapp", "build");
 
+    var pgUser = builder.AddParameter("postgres-user", "articler");
+    var pgPassword = builder.AddParameter("postgres-password", "articler", secret: true);
+    var postgres = builder
+        .AddPostgres("postgres", port: 5432)
+        .WithEnvironment("POSTGRES_USER", "articler") // for scripts
+        .WithEnvironment("POSTGRES_PASSWORD", "articler") // for scripts
+        .WithEnvironment("POSTGRES_DB", "articler") // for scripts
+        .WithUserName(pgUser)
+        .WithPassword(pgPassword)
+        .WithDataVolume("articler_postgres")
+        .WithBindMount("Scripts", "/docker-entrypoint-initdb.d")
+        .AddDatabase("articler");
+
+    var siloHost = builder.AddDockerfile("articler-silohost", "..", "Articler.SiloHost/Dockerfile")
+        .WithHttpEndpoint(port: 11111, targetPort: 11111, name: "silo")
+        .WithHttpEndpoint(port: 30000, targetPort: 30000, name: "gateway")
+        .WithEnvironment("ASPNETCORE_ENVIRONMENT", isDevelopmentMode ? "Development" : "Docker")
+        .WithEnvironment("DOTNET_ENVIRONMENT", isDevelopmentMode ? "Development" : "Docker")
+        .WithEnvironment("ORLEANS_SILO_PORT", "11111")
+        .WithEnvironment("ORLEANS_GATEWAY_PORT", "30000")
+        .WithEnvironment("POSTGRES_CONNECTION_STRING", "Host=postgres;Database=articler;Username=articler;Password=articler")
+        .WaitFor(postgres);
+
+    // Start Web API in Docker
+    var webApi = builder.AddDockerfile("articler-webapi", "..", "Articler.WebApi/Dockerfile")
+        .WaitFor(webAppBuild)
+        .WaitFor(siloHost)
+        .WithHttpEndpoint(port: 8080, targetPort: 8080, name: "http")
+        .WithEnvironment("ASPNETCORE_ENVIRONMENT", isDevelopmentMode ? "Development" : "Docker")
+        .WithEnvironment("DOTNET_ENVIRONMENT", isDevelopmentMode ? "Development" : "Docker")
+        .WithEnvironment("ASPNETCORE_URLS", "http://+:8080")
+        .WithEnvironment("POSTGRES_CONNECTION_STRING", "Host=postgres;Database=articler;Username=articler;Password=articler");
 }
 else
 {
